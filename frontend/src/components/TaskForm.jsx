29cdefb38cbddca39ecfddb7d3316602f121bfc6
jhadeo <105715821+jhadeo@users.noreply.github.com>
2026-08-05 21:51:06 +0800

    feat: group labels and inputs for TaskForm component

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -3,10 +3,31 @@ export function TaskForm({ handleSubmit, title, desc, setTitle, setDesc }) {
     <>
       <form onSubmit={handleSubmit}>
         <h1>create task</h1>
-        <label htmlFor="title">Title</label>
-        <input type="text" name="title" id="" value={title} onChange={(e)=>{setTitle(e.target.value)}} />
-        <label htmlFor="desc">Description</label>
-        <input type="text" name="desc" id="" value={desc}onChange={(e)=>{setDesc(e.target.value)}} />
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            id=""
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="desc">Description</label>
+          <input
+            type="text"
+            name="desc"
+            id=""
+            value={desc}
+            onChange={(e) => {
+              setDesc(e.target.value);
+            }}
+          />
+        </div>
         <button type="submit">Submit</button>
       </form>
     </>
